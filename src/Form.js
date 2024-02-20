887449649c351be5c1a805abd8cd23aedbd7fe98
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-02-20 15:40:07 +0100

    make placeholder in inputfield

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Form({ handleAddTodo, todoNameRef }) {
   return (
     <div className="Body">
-      <input ref={todoNameRef} type="text"></input>
+      <input ref={todoNameRef} type="text" placeholder="Type here"></input>
       <br></br>
       <button onClick={handleAddTodo}>Submit</button>
     </div>
