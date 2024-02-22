7ae9145dd444684ccc80f7788d6ce23e08c587f5
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-02-22 16:55:29 +0100

    Store todos with useEffect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import './style.css';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Header from './Header.js';
 import Form from './Form.js';
 import ToDoList from './ToDoList.js';
 
+const LSKEY = 'MyTodoApp';
+
 function App() {
   const [ToDos, setTodos] = useState([
-    { name: 'Laundry', completed: false },
-    { name: 'Clean kitchen', completed: false },
+    { id: 1, name: 'Laundry', completed: false },
+    { id: 2, name: 'Clean kitchen', completed: false },
   ]);
 
   const todoNameRef = useRef();
 
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LSKEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem(LSKEY + '.ToDos', JSON.stringify(ToDos));
+  }, [ToDos]);
+
   function handleAddTodo() {
     const name = todoNameRef.current.value;
     if (name === '') return;
-    setTodos((prevTodos) => [...prevTodos, { name: name, completed: false }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: uuidv4(), name: name, completed: false },
+    ]);
     todoNameRef.current.value = '';
   }
 
