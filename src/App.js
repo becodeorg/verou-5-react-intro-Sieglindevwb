486fdf2bf6b13615f0ebe14238f8b0dc8ec10016
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-02-22 15:22:57 +0100

    Make checkbox work

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import Form from './Form.js';
 import ToDoList from './ToDoList.js';
 
 function App() {
-  const [ToDos, setTodos] = useState(['Laundry', 'Clean kitchen']);
+  const [ToDos, setTodos] = useState([
+    { name: 'Laundry', completed: false },
+    { name: 'Clean kitchen', completed: false },
+  ]);
 
   const todoNameRef = useRef();
 
-  function handleAddTodo(e) {
+  function handleAddTodo() {
     const name = todoNameRef.current.value;
     if (name === '') return;
-    setTodos((prevTodos) => [...prevTodos, name]);
+    setTodos((prevTodos) => [...prevTodos, { name: name, completed: false }]);
     todoNameRef.current.value = '';
   }
+
+  function toggleTodo(index) {
+    const newTodos = [...ToDos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
       <Header />
       <Form handleAddTodo={handleAddTodo} todoNameRef={todoNameRef} />
-      <ToDoList ToDos={ToDos} />
+      <ToDoList ToDos={ToDos} toggleTodo={toggleTodo} />
     </div>
   );
 }
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
-function ToDoList({ ToDos }) {
+function ToDoList({ ToDos, toggleTodo }) {
   return (
     <div>
       <h2>To do list:</h2>
-      <ul>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
         {ToDos.map((ToDo, index) => (
-          <li key={index}>
-            <input type="checkbox" />
-            {ToDo}
+          <li
+            key={index}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              marginBottom: '2px',
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={ToDo.completed}
+              onChange={() => toggleTodo(index)}
+            />
+            <span
+              style={{
+                textDecoration: ToDo.completed ? 'line-through' : 'none',
+                marginLeft: '10px',
+              }}
+            >
+              {ToDo.name}
+            </span>
           </li>
         ))}
       </ul>
